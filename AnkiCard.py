4bce5a6535fc9943d915f23e4e35f5ee20b01495
khunguraharman <42793056+khunguraharman@users.noreply.github.com>
2026-01-03 18:06:33 -0800

    Update AnkiCard.py

--- AnkiCard.py
+++ AnkiCard.py
@@ -4,7 +4,11 @@ from typing import ClassVar, Union
 import re
 
 @dataclass
-class BasicAnkiCard:
+class AnkiCard:
+    pass
+
+@dataclass
+class BasicAnkiCard(AnkiCard):
     front: str
     back: list[str]
 
@@ -16,85 +20,45 @@ class RawClozeAnkiCard:
     clozeFragments: list[str]
 
 @dataclass
-class ClozeAnkiCard:
-    fullText: str
+class ClozeAnkiCard(AnkiCard):
+    headers: list[str]
     clozeDeletions: list[str]
 
-# def is_front_of_card(first_char: str) -> bool:
-#     # Implement logic to determine if a string is the front of an Anki card    
-#     if first_char.isupper() and 'A' <= first_char <= 'Z':
-#         return True
-#     else:
-#         return False
+def is_front_of_card(first_char: str) -> bool:
+    # Implement logic to determine if a string is the front of an Anki card    
+    if first_char.isupper() and 'A' <= first_char <= 'Z':
+        return True
+    else:
+        return False
 
-# def check_in_table_block(line: str, inTable: bool, dims: tuple[int, int]) -> tuple[bool, tuple[int, int]]:
-#     if line.startswith(AnkiCard.tableStart):
-#         rowcol = get_table_dimensions(line)
-#         #print(f"Assigned dims: {rowcol}")
-#         #print(f"DETECTED A TABLE: {line}")
-#         return (True, rowcol)
-#     elif line.startswith(AnkiCard.tableEnd):
-#         rowcol = (0, 0)
-#         #print(f"Assigned dims: {rowcol}")
-#         #print(f"NOT A TABLE: {line}")
-#         return (False, rowcol)
-#     else:
-#         #print(f"No Change to dims: {dims}")
-#         #print(f"NO CHANGE: {line}")
-#         return (inTable, dims)
+def create_basic_cards(doc_path: str) -> list[BasicAnkiCard]:
+    anki_cards = []
+    with open(doc_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+    front = lines[0]
+    back = []
+    for line in lines[1:]:        
+        first_char = line[0]
+        if not is_front_of_card(first_char):
+            back.append(line)
+        else:
+            anki_cards.append(BasicAnkiCard(front, back))
+            front = line
+            back = []
+    return anki_cards
 
-# def get_table_dimensions(line: str) -> tuple[int, int]:
-#     match = re.search(r'(\d+)x(\d+)$', line)
-#     if match:
-#         dims = tuple(map(int, match.groups()))
-#         return dims
-#     else:
-#         return tuple(0, 0)
+def create_cloze_cards(doc_path: str) -> list[ClozeAnkiCard]:
+    anki_cards:list[ClozeAnkiCard] = []
+    with open(doc_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+    for i in range(0, len(lines), 2):
+        headers: list[str] = lines[i].split('\t')
+        fragments = lines[i+1].split('\t')
+        anki_cards.append(ClozeAnkiCard(headers, clozeDeletions = fragments) )       
+    return anki_cards
 
-# def handle_table_block(line: str, dims: tuple[int, int]) -> TableBlock:
-#     #print(line)
-#     #print(dims)
-#     cells = line.split('\t')
-#     table:list[list[str]] = []
-#     for row in range(0, dims[0]): #iterate 0,1,2
-#         cols = []
-#         for col in range(0, dims[1]): #iterate 0,1,2
-#             index = 2*row + col + row
-#             cols.append(cells[index])
-#             print(cells[index])
-#         table.append(cols)
-#     return table
-
-# def create_anki_cards(doc_path: str) -> list[AnkiCard]:
-#     anki_cards = []
-#     with open(doc_path, 'r', encoding='utf-8') as file:
-#         lines = file.readlines()
-#     front = lines[0].strip()
-#     back = []
-#     dims = (0,0)
-#     inTable = False
-#     for line in lines[1:]:
-#         clean = line.strip()
-#         first_char = clean[0]
-#         (inTable, dims) = check_in_table_block(clean, inTable, dims)
-#         # if inTable = True, must append to back
-#         if inTable:
-#             if clean.startswith(AnkiCard.tableStart):
-#                 continue            
-#             back.append(handle_table_block(line, dims))
-#         #create a new card and reinitialize front and back
-#         elif not inTable and not is_front_of_card(first_char):
-#             if clean.startswith(AnkiCard.tableEnd):
-#                 continue
-#             back.append(TextBlock([clean]))
-#         else:
-#             anki_cards.append(AnkiCard(front, back))
-#             front = clean
-#             back = []
-#     return anki_cards
-
-# if __name__ == "__main__":
-#     doc_path = "raw_anki_cards_w_tables.txt"
-#     anki_cards = create_anki_cards(doc_path)
-#     for card in anki_cards:
-#         print(card)
+if __name__ == "__main__":
+    doc_path = "basic_anki_cards.txt"
+    basic_anki_cards = create_basic_cards(doc_path)
+    for card in basic_anki_cards:
+        print(card)
