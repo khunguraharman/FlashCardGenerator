3c95c363fb1e969f50508cd36376169803bbdd5f
khunguraharman <khungura_harman@hotmail.com>
2026-01-31 18:29:37 -0800

    removed untracked cached files

--- IterateThroughSections.py
+++ IterateThroughSections.py
@@ -14,7 +14,7 @@ def analyze_document() -> None:
     document_intelligence_client = DocumentIntelligenceClient(endpoint, credential)
     with open(doc_path, "rb") as f:
         poller = document_intelligence_client.begin_analyze_document(
-            model_id=model_id,body=AnalyzeDocumentRequest(bytes_source=f.read()), pages="269"
+            model_id=model_id,body=AnalyzeDocumentRequest(bytes_source=f.read()), pages="239-240"
         )
     result = poller.result()
 
